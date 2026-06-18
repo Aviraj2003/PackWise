c2d69f3077fb47bd142629d235746efced84bbd8
Aviraj2003 <149134035+Aviraj2003@users.noreply.github.com>
2026-06-18 12:15:58 +0530

    Refactor app.py for PackWise 3D Box Checker

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ import plotly.graph_objects as go
 # =========================================================
 
 st.set_page_config(
-    page_title="Fast 3D Box Fitness Checker",
+    page_title="PackWise",
     layout="wide"
 )
 
-st.title("📦 Fast 3D Box Fitness Checker")
+st.title("📦 PackWise-3D Box Fitness Checker")
 
 st.write(
     "Automatically finds the best rotation "
